c7a7b072e4c4b990870a41c5c4248f807185e234
許家榮 <jackeett@gmail.com>
2020-12-25 11:58:46 +0800

    更新HC6600通訊

--- ChargingPileSystem/Protocols/ElectricMeter/HC6600Protocol.cs
+++ ChargingPileSystem/Protocols/ElectricMeter/HC6600Protocol.cs
@@ -14,8 +14,9 @@ namespace ChargingPileSystem.Protocols.ElectricMeter
             try
             {
                 int Index = 0;
-                ushort[] data = master.ReadHoldingRegisters(ID, 0x64, 66);
-                ushort[] data1 = master.ReadInputRegisters(ID, 324, 13);
+                ushort[] data = master.ReadHoldingRegisters(ID, 100, 44);
+                ushort[] data1 = master.ReadHoldingRegisters(ID, 324, 13);
+                ushort[] data2 = master.ReadHoldingRegisters(ID, 368, 6);
                 RSv = MathClass.work16to754(data[Index + 1], data[Index]); Index += 2;
                 STv = MathClass.work16to754(data[Index + 1], data[Index]); Index += 2;
                 TRv = MathClass.work16to754(data[Index + 1], data[Index]); Index += 2;
@@ -39,22 +40,114 @@ namespace ChargingPileSystem.Protocols.ElectricMeter
                 Sv = MathClass.work16to754(data[Index + 1], data[Index]); Index += 2;
                 Tv = MathClass.work16to754(data[Index + 1], data[Index]);
                 Index = 0;
-                HZ = (data1[Index] * 0.1f); Index++;
-                kW_A = (data1[Index] * 0.01f); Index++;
-                kVAR_A = (data1[Index] * 0.01f); Index++;
-                kVA_A = (data1[Index] * 0.1f); Index++;
-                PF_A = (data1[Index] * 0.01f); Index++;
-                kW_B = (data1[Index] * 0.01f); Index++;
-                kVAR_B = (data1[Index] * 0.01f); Index++;
-                kVA_B = (data1[Index] * 0.1f); Index++;
-                PF_B = (data1[Index] * 0.01f); Index++;
-                kW_C = (data1[Index] * 0.01f); Index++;
-                kVAR_C = (data1[Index] * 0.01f); Index++;
-                kVA_C = (data1[Index] * 0.1f); Index++;
-                PF_C = (data1[Index] * 0.01f);
-                kWh_A = kWh;
-                kWh_B = kWh;
-                kWh_C = kWh;
+                if (data1[Index] > 32767)
+                {
+                    HZ = (data1[Index] - 65536) * 0.002f; Index++;
+                }
+                else
+                {
+                    HZ = (data1[Index] * 0.002f); Index++;
+                }
+                if (data1[Index] > 32767)
+                {
+                    kW_A = (data1[Index] - 65536) * 0.2f; Index++;
+                }
+                else
+                {
+                    kW_A = (data1[Index] * 0.2f); Index++;
+                }
+                if (data1[Index] > 32767)
+                {
+                    kVAR_A = (data1[Index] - 65536) * 0.2f; Index++;
+                }
+                else
+                {
+                    kVAR_A = (data1[Index] * 0.2f); Index++;
+                }
+                if (data1[Index] > 32767)
+                {
+                    kVA_A = (data1[Index] - 65536) * 0.2f; Index++;
+                }
+                else
+                {
+                    kVA_A = (data1[Index] * 0.2f); Index++;
+                }
+                if (data1[Index] > 32767)
+                {
+                    PF_A = (data1[Index] - 65536) * 0.0001f; Index++;
+                }
+                else
+                {
+                    PF_A = (data1[Index] * 0.0001f); Index++;
+                }
+                if (data1[Index] > 32767)
+                {
+                    kW_B = (data1[Index] - 65536) * 0.2f; Index++;
+                }
+                else
+                {
+                    kW_B = (data1[Index] * 0.2f); Index++;
+                }
+                if (data1[Index] > 32767)
+                {
+                    kVAR_B = (data1[Index] - 65536) * 0.2f; Index++;
+                }
+                else
+                {
+                    kVAR_B = (data1[Index] * 0.2f); Index++;
+                }
+                if (data1[Index] > 32767)
+                {
+                    kVA_B = (data1[Index] - 65536) * 0.2f; Index++;
+                }
+                else
+                {
+                    kVA_B = (data1[Index] * 0.2f); Index++;
+                }
+                if (data1[Index] > 32767)
+                {
+                    PF_B = (data1[Index] - 65536) * 0.0001f; Index++;
+                }
+                else
+                {
+                    PF_B = (data1[Index] * 0.0001f); Index++;
+                }
+                if (data1[Index] > 32767)
+                {
+                    kW_C = (data1[Index] - 65536) * 0.2f; Index++;
+                }
+                else
+                {
+                    kW_C = (data1[Index] * 0.2f); Index++;
+                }
+                if (data1[Index] > 32767)
+                {
+                    kVAR_C = (data1[Index] - 65536) * 0.2f; Index++;
+                }
+                else
+                {
+                    kVAR_C = (data1[Index] * 0.2f); Index++;
+                }
+                if (data1[Index] > 32767)
+                {
+                    kVA_C = (data1[Index] - 65536) * 0.2f; Index++;
+                }
+                else
+                {
+                    kVA_C = (data1[Index] * 0.2f); Index++;
+                }
+                if (data1[Index] > 32767)
+                {
+                    PF_C = (data1[Index] - 65536) * 0.0001f; Index++;
+                }
+                else
+                {
+                    PF_C = (data1[Index] * 0.0001f); Index++;
+                }
+                Index = 0;
+                kWh_A = MathClass.work16to10(data2[Index], data2[Index + 1]) * 0.001; Index += 2;
+                kWh_B = MathClass.work16to10(data2[Index], data2[Index + 1]) * 0.001; Index += 2;
+                kWh_C = MathClass.work16to10(data2[Index], data2[Index + 1]) * 0.001;
                 ConnectFlag = true;
             }
             catch (Exception ex)
